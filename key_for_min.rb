1cc22024e9b36f6b21954febff1d6572349e696c
Domenico Cascone <dcascone84@gmail.com>
2020-03-13 16:11:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 def key_for_min_value(name_hash)
   smallest_name = nil
   smallest_num = Float::INFINITY
-  name_hash.each do |name, num|
+  name_hash.collect do |name, num|
     if num < smallest_num
       smallest_name = name
       smallest_num = num
